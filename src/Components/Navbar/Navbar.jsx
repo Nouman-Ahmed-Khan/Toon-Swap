ab0329e7676c6664b580923436aa782ba507b708
Nouman Ahmed Khan <noumanahmedkhan834@gmail.com>
2025-06-27 23:10:19 +0500

    Background image added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,20 +4,24 @@ import logo from '../../assets/logo.png'
 import telegram_icon from '../../assets/telegram_icon.svg'
 import x_icon from '../../assets/x_icon.svg'
 
+
 const Navbar = () => {
   return (
-    <div className='navbar flex items-center justify-around p-4 shadow-md mt-5 mx-15 rounded-2xl'>
+    <div>
+      <div className='navbar border-3 flex items-center justify-around py-4 px-0 shadow-md mt-5 mx-20 rounded-2xl'>
+      <div className='flex items-center justify-between max-w-3xl w-full'>
       <div className="nav-logo">
         <img src={logo} alt="logo" className='w-50'/>
       </div>
 
       <div>
-       <ul className='nav-links flex items-center gap-8 px-4 py-1 rounded-xl border-2'>
+       <ul className='nav-links flex items-center gap-8 px-2 py-1 rounded-xl border-2'>
          <li className='nav-links cursor-pointer font-semibold text-sm px-4 py-2 rounded-xl active'>Home</li>
          <li className='cursor-pointer font-semibold text-sm px-4 py-2 rounded-xl'>Toon Pad</li>
          <li className='cursor-pointer font-semibold text-sm px-4 py-2 rounded-xl'>Leaderboard</li>
          <li className='cursor-pointer font-semibold text-sm px-4 py-2 rounded-xl'>Docs</li>
        </ul>
+      </div>
       </div>
       
       <div className="nav-contact flex gap-4 items-center">
@@ -27,6 +31,12 @@ const Navbar = () => {
          </div>
          <button type="button" className='connect-wallet-button rounded-xl px-3 py-2 cursor-pointer font-semibold text-sm border-2'>Connect Wallet</button>
       </div>
+    </div>
+
+    <div className='navbar border-3 flex items-center justify-around py-4 px-0 shadow-md mt-5 mx-20 rounded-2xl'>
+      
+    </div>
+
     </div>
   )
 }
