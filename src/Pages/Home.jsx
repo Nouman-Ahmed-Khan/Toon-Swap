fd0bf4ada1c9cdb036d51aa693e85a8548e8ea30
Nouman Ahmed Khan <noumanahmedkhan834@gmail.com>
2025-07-08 23:44:30 +0500

    work started in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import logo from '../assets/circlelogo.png'
+import swaplogo from '../assets/swaplogo.png'
 
 const Home = () => {
   return (
     <div className='mx-auto px-4 sm:px-12 mt-15'>
       <div className='navbar rounded-xl'>
-        <div className='my-10 nav-links-mainbg'>
-          <h1>Home</h1>
+        CAT
+        <div className='my-10 nav-links-mainbg mx-10 border-3 rounded-xl py-3'>
+          {/* toonswap and other logo start*/}
+          <div className='flex'>
+            <img src={logo} alt="logo" className='w-25 sm:w-9 cursor-pointer'/>
+            <img src={swaplogo} alt="logo" className='w-25 sm:w-9 cursor-pointer'/>
+          </div>
+          {/* toonswap and other logo end*/}
         </div>
       </div>
     </div>
