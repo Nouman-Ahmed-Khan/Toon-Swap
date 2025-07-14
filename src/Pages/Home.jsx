4efaaa1865533bce37ff141d2090cab82278b569
Nouman Ahmed Khan <noumanahmedkhan834@gmail.com>
2025-07-14 23:18:08 +0500

    working on homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -72,9 +72,9 @@ const Home = () => {
 
 
         {/* Home Page Section-1 Start */}
-         <div>
+         <div className='flex flex-col sm:flex-row items-center sm:my-4'>
            {/* Pair info card Start */}
-           <div className='nav-links-mainbg sm:mx-5 sm:p-4 mt-5 border-3 rounded-xl'>
+           <div className='sm:w-[30%] nav-links-mainbg sm:mx-5 sm:p-4 border-3 rounded-xl'>
              <p className='text-2xl text-white font-bold'>Pair Info</p>
 
              {/* Toon and Supra section Start */}
@@ -124,6 +124,50 @@ const Home = () => {
 
            </div>
            {/* Pair info card End */}
+
+           {/* Your Positions card Start */}
+           <div className='sm:w-[70%] nav-links-mainbg sm:mr-5 sm:p-4 border-3 rounded-xl'>
+             <div>
+              <p className='text-2xl text-white font-bold'>Your Positions</p>
+              {/* Active inactive buttons star */}
+              <div className='navbar rounded-xl border-3 sm:p-2 sm:mt-4'>
+               
+                {/* supra start */}
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center justify-center'>
+                    <img src={swaplogo} alt="logo" className='w-25 sm:w-8 cursor-pointer'/>
+                    <p className='font-bold ml-3'>SUPRA</p>
+                  </div>                  
+                  <p>~157.006 TOON</p>
+                </div>
+                {/* supra end */}
+              </div>
+              {/* Active inactive buttons end */}
+             </div>      
+             
+              <div className='text-sm'>
+                  <div className='flex items-center justify-between mt-4 mx-2'>
+                    <p>Liquidity / TVL</p>
+                    <p>$ 94.073 k</p>
+                  </div>
+                  <div className='flex items-center justify-between mt-1 text-gray-300 mx-4'>
+                    <p>TOON Share</p>
+                    <p>$ 64.004 k</p>
+                  </div>
+                  <div className='flex items-center justify-between mt-1 text-gray-300 mx-4'>
+                    <p>SUPRA Share</p>
+                    <p>$ 30.890 k</p>
+                  </div>
+                  <div className='flex items-center justify-between mt-2 text-green mx-2'>
+                    <p>Fee 24h</p>
+                    <p>$ 200.564 k</p>
+                  </div>
+              </div>
+
+              <button className='mt-4 w-full rounded-xl px-3 py-2 cursor-pointer font-semibold text-sm border-2 connect-wallet-button'>Add Liquidity</button>
+
+           </div>
+           {/* Your Positions card End */}
          </div>
         {/* Home Page Section-1 End */}
       </div>    
