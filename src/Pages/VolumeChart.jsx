754d11a2e449b7abdced33930d215cb182606bde
Nouman Ahmed Khan <noumanahmedkhan834@gmail.com>
2025-08-23 13:57:28 +0500

    volumn file update

--- src/Pages/VolumeChart.jsx
+++ src/Pages/VolumeChart.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
-
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Legend,
-  Tooltip,
-} from 'chart.js';
-
 import { Chart } from 'react-chartjs-2';
 
-// All chart controllers and elements MUST be registered here.
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  Legend,
-  Tooltip
-);
+// Import the 'auto' package to automatically register all chart components.
+import 'chart.js/auto'; 
 
 const VolumeChart = () => {
   const labels = [
